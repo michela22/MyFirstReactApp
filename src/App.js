5f18525d3dda530a1f6af28c12024a60d08fd263
michela22 <michela.minniti@libero.it>
2021-12-16 18:05:01 +0100

    function ElementoLista

--- src/App.js
+++ src/App.js
@@ -3,19 +3,35 @@ import './App.css';
 import User from './User'; //faccio innesto di componente dentro un altro componente
 import HelloWorld from './HelloWorld';
 
+function ElementoLista(props){
+   /* vado ad iterare sugli elementi del mio array , map da la possibilità di iterare sugli elementi di un array
+  e ritornare del codice html dove è possibile accedere all'elemtno i-esimo della iterazione*/
+  return <li>{props.elemento}</li>          
+}
+
 function App() { /*per indicare le classi css usa className e non class */
 
   const name = "Michela";
-  const otherName= "Giovanni";
+  const otherName= "Giulio";
+
+  const lista =["Mele","Pere", "Patate"];
+  
+
 
   return (
     <div className="App"> 
     
-      <header className="App-header">
+    <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
     {/* <User user="user"/>*/}
         <User user={name} />
         <HelloWorld hello={otherName} />
+
+       <ul>
+          {lista.map((elemento) => {  {/* è come ngFor di Angular*/}
+          return <ElementoLista elemento = {elemento} />
+         })}
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
